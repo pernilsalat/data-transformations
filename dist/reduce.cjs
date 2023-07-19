478d33d0780e208816887a032c4bc1848f6ac619
pernilsalat <jazzel88@gmail.com>
2023-07-19 18:09:28 +0200

    Delete dist directory

--- dist/reduce.cjs
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-require("./curry.cjs");
-require("./get-6c5350df.cjs");
-const reduce = require("./reduce-ff3d25ca.cjs");
-require("./is.cjs");
-require("./complement.cjs");
-exports.reduce = reduce.reduce;
-//# sourceMappingURL=reduce.cjs.map
--- dist/reduce.js
+++
@@ -1,9 +0,0 @@
-import "./curry.js";
-import "./get-3e246b66.js";
-import { r } from "./reduce-86de5073.js";
-import "./is.js";
-import "./complement.js";
-export {
-  r as reduce
-};
-//# sourceMappingURL=reduce.js.map
